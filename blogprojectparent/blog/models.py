6b04cdc2ca1da433885f74fd02827a2de4b32cf1
TaeeunKim <xodms0309@naver.com>
2021-05-10 21:35:25 +0900

    블로그 프로젝트 업데이트

---
+++ blogprojectparent/blog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class HashTag(models.Model):
+    hashtag_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.hashtag_name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('date published')
+    contents = models.TextField()
+    hashtag = models.ManyToManyField(HashTag)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.contents[:100]
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Blog, related_name='comments', on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=20)
+    comment_text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def approve(self):
+        self.save()
+
+    def __str__(self):
+        return self.comment_text
